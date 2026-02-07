f0ce2fc79a6bd1e5b001c8de91fc3d17fcd8239f
DESKTOP-EJ7G0LD\karsi <karsiang@hotmail.com>
2026-02-07 11:39:01 -0700

    CSC-505 week8 assignment Final Project - ATM model application python

---
+++ CSC-505-week8_assignment-fp/atm_script.py
@@ -0,0 +1,299 @@
+############### PHTRS program start ###################################################
+import sys # import basic sys library
+import calendar #import calendar library to prettify the dates
+import os # checks the file path to ensure the account exists
+from datetime import datetime #import the date time library
+    
+current_year = datetime.now().year #prints the current year
+current_month = datetime.now().month #prints the current month
+current_day = datetime.now().day # prints the current day
+current_month_name = calendar.month_name[current_month] # prints current month name
+    
+def userwishestoexit(): # function is used for exiting the program 
+    try:
+        user_input = input("Press Enter to continue or Type Q to exit program! ")
+        if user_input.upper() == 'Q': 
+            print("Program will now exit...Thank you!")
+            sys.exit() # Exit gracefully
+        else:
+            return True # return to main for flow of execution
+    except Exception as e:
+        print(f"An error occurred within func-userwishestoexit(): {e}. Exiting program.") 
+
+def requeststrinput(verbiage,inputtype,retry): # generic function defined to capture string type inputs or prompts
+    try:        
+        if retry <3:
+            checkinginputval = True
+            inputval = input(verbiage) #actual verbiage is getting passed into the function for prompting the user        
+        else:
+            checkinginputval = False
+
+        while checkinginputval:
+
+            if inputtype == "":
+                if inputval!="":                
+                    if inputval.lower() == 'q': # if the user enters a q or Q the program ends
+                        checkinginputval=False                           
+                    else:
+                        return inputval # this is for user entering as long as non empty string 
+                else:
+                    print("Sorry! You have not entered anything yet. ") # user prompt for empty string
+                    retry = retry+1
+                    inputval = requeststrinput(f"{verbiage}","", retry)  
+                    
+            elif inputtype == "menuopt":
+                if inputval!="":
+                    if inputval.lower() == 'q': # if the user enters a q or Q the program ends
+                        checkinginputval=False                           
+                    else:
+                        if inputval.isdigit() and int(inputval) > 0 and int(inputval) < 5  :
+                            return inputval 
+                        else:
+                            print("Sorry! Your entry is Invalid. ") # user prompt for empty string
+                            retry = retry+1
+                            inputval =requeststrinput(f"{verbiage}","menuopt", retry)  
+                else:
+                    print("Sorry! You have not entered any options. ") # user prompt for empty string
+                    retry = retry+1
+                    inputval =requeststrinput(f"{verbiage}","digit", retry)  
+                    #inputval = input(verbiage)
+            elif inputtype == "digit":
+                if inputval!="":
+                    if inputval.lower() == 'q': # if the user enters a q or Q the program ends
+                        checkinginputval=False                           
+                    else:
+                        if inputval.isdigit() and int(inputval) > 0:
+                            return inputval 
+                        else:
+                            print("Sorry! Your entry is Invalid. ") # user prompt for empty string
+                            retry = retry+1
+                            inputval =requeststrinput(f"{verbiage}","digit", retry)  
+                            #inputval = input(verbiage)
+                else:
+                    print("Sorry! You have not entered any options. ") # user prompt for empty string
+                    retry = retry+1
+                    inputval =requeststrinput(f"{verbiage}","digit", retry)  
+                    #inputval = input(verbiage)
+            elif inputtype == "pin":
+                if inputval!="":
+                    if inputval.lower() == 'q': # if the user enters a q or Q the program ends
+                        checkinginputval=False                           
+                    else:
+                        if inputval.isdigit() and int(inputval) > 0 and len(inputval) == 4: #Pins are 4digit long
+                            return inputval 
+                        else:
+                            print("Sorry! Your entry is Invalid! ") # user prompt for empty string
+                            retry = retry+1
+                            inputval =requeststrinput(f"{verbiage}","pin", retry)
+                            #inputval = input(verbiage)
+                else:
+                    print("Sorry! You have not entered any options. ") # user prompt for empty string
+                    retry = retry+1
+                    inputval =requeststrinput(f"{verbiage}","pin", retry)
+                    #inputval = input(verbiage)
+        else:
+            print(f"Cancelling and Exiting program...Thank you!")
+            sys.exit() # calling system.exit                       
+    except Exception as e:
+        print(f"An error occurred within func-requeststrinput(): {e}. Exiting program.") 
+
+def atm_menu(account,cardnumber):
+    try:
+       
+        menuopt = requeststrinput(f" Enter [1] for Withdrawal| [2] for Deposits | [3] For Account Balance | Your selection = | [4] To Exit ","menuopt",0)   
+        match menuopt:
+            case "1":
+                print("Process Withdrawal************************")
+                amountwithdraw = requeststrinput("Please enter your $$amount to withdraw (Note: Only full amounts no Cents): ","digit", 0)
+                if int(amountwithdraw) > int(account.accbalance):
+                    print(f"Sorry you've exceeded the limit of the withdrawal!")
+                elif int(amountwithdraw) == int(account.accbalance):
+                    withdrawupdateaccount(account,amountwithdraw,cardnumber)
+                    print(f"Success! ${amountwithdraw} has been withdrawn. Your account will be closed! Thank you!")
+                    sys.exit() # system exit
+                else:
+                    withdrawupdateaccount(account,amountwithdraw,cardnumber)
+                    print(f"Success! ${amountwithdraw} has been withdrawn.")
+          
+            case "2":
+                print("Process Deposit************************")
+                amountdeposit = requeststrinput("Please enter the $$amount to deposit (Note: Only full amounts no Cents): ","digit", 0)
+               
+                depositupdateaccount(account,amountdeposit,cardnumber)
+                print(f"Success! ${amountdeposit} has been deposited.")
+          
+            case "3":
+                print("Print Balance************************")
+                accountobj = load_accountinformation(cardnumber)
+                print(f"Your Current Balance is: ${accountobj.accbalance}")
+                print("Thank you!! ***********")
+                
+            case "4":
+                print("Process Exit************************")
+                print("Thank you!! Please Come Back another time!***********")
+                sys.exit() # system exit
+            case _:
+                print(f"*******************************************")
+
+        atm_menu(account,cardnumber)
+    except Exception as e:
+        print(f"An error occurred within func-atm_menu(): {e}. Exiting program.")
+        sys.exit() # system exit
+
+        
+def atm_demo_proto(customer): # main python program    
+    try:        
+        whileuserunexited = userwishestoexit()
+        """Renders the app continues           
+            """
+        while whileuserunexited:       
+            if customer == "" : 
+                cardnumber = requeststrinput("Please insert your account card number: ","digit", 0)  
+
+                if check_ifaccountexists(cardnumber) == True:
+                    accountobj = load_accountinformation(cardnumber)
+                   
+                    authsuccess = authenticate_user(accountobj,0)
+                    
+                    if authsuccess == True:
+                        isactive = check_ifaccountisactive(accountobj)
+                        if isactive == True:
+                            print(f"*******************************************")
+                            print(f"Welcome {accountobj.loginuser}! {current_day} {current_month_name.upper()}, {current_year}")
+                            print(f"*******************************************")
+                            atm_menu(accountobj,cardnumber)
+                        else:
+                            print(f"Your account is Inactive $0 ! Exitting program.")
+                            sys.exit() # system exit                       
+                    else:
+                        print(f"Your account is Invalid! Exitting program.")
+                        sys.exit() # system exit
+                else:
+                    print(f"Your account is Invalid! Exitting program.")
+                    sys.exit() # system exit
+                        
+    except Exception as e:
+        print(f"An error occurred within func-atm_demo_proto(): {e}. Exiting program.")
+        sys.exit() # system exit
+
+
+##################### program main ##################################################
+
+def check_ifaccountisactive(account):
+    try:
+        if int(account.accbalance) >0 :
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"An error occurred within check_ifaccountexists(): {e}. Exiting program.")        
+  
+def authenticate_user(account,pinattempts):
+    try:
+        if pinattempts<3:
+            pinnumber = requeststrinput(f"Please enter your Pin: ","pin", 0)
+            if account.loginpass == pinnumber:
+                return True
+            else:
+                pinattempts = pinattempts+1
+                authenticate_user(account,pinattempts)
+        else:
+            return False        
+    except Exception as e:
+        print(f"An error occurred within authenticate_user(): {e}. Exiting program.")        
+      
+def check_ifaccountexists(cardnumber):
+    try:
+        accountfile = "aci_"+cardnumber+".txt" 
+        if os.path.isfile(accountfile):
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"An error occurred within check_ifaccountexists(): {e}. Exiting program.")        
+  
+def load_accountinformation(cardnumber):
+    try:
+        accountfile = "aci_"+cardnumber+".txt" 
+        with open(accountfile, "r") as file:
+            contentline = file.readline()
+
+        if len(contentline) != 0:
+            rconst = contentline.split("|")
+            if len(rconst) > 1:
+                customerobj = account() 
+                               
+                customerobj.loginuser = rconst[0]
+                customerobj.loginpass = rconst[1]                 
+                customerobj.accbalance = rconst[2]
+                                
+                return customerobj                 
+    except Exception as e:
+        print(f"An error occurred within func1-load_accountinformation(): {e}. Exiting program.")        
+
+def updateaccountfile(self, cardnumber, newbalance):
+    try:               
+        accountfile = "aci_"+cardnumber+".txt" 
+        with open(accountfile, "r") as file:
+            contentline = file.readline()
+
+        if len(contentline) != 0:
+            rconst = contentline.split("|")
+            if len(rconst) > 1:
+                customerobj = account() 
+                               
+                customerobj.loginuser = rconst[0]
+                customerobj.loginpass = rconst[1]                 
+                customerobj.accbalance = newbalance
+                newcontentline =   customerobj.loginuser+"|"+customerobj.loginpass+"|"+str(customerobj.accbalance)               
+                with open(accountfile, "w") as file:                    
+                    file.write(newcontentline)
+
+                return customerobj   
+    except Exception as e:
+        print(f"An error occurred within func1-updateaccountfile(): {e}. Exiting program.")
+       
+class account: #defines the account class
+    def __init__(self, loginuser="", loginpass="",accbalance=""):         
+        self.loginuser = loginuser
+        self.loginpass = loginpass
+        self.accbalance = accbalance       
+
+
+def withdrawupdateaccount(self, amount, cardnumber):      
+    try:
+        balance =  self.accbalance
+        newbalance = int(balance)-int(amount)
+        self.accbalance = newbalance
+        print(f"Your New balance is ${self.accbalance} ")       
+
+        updatedcustomerobj = updateaccountfile(self, cardnumber, newbalance)
+
+        return updatedcustomerobj
+    
+    except Exception as e:
+        print(f"An error occurred within func1-load_withdrawupdateaccount(): {e}. Exiting program.")
+
+
+def depositupdateaccount(self, amount, cardnumber):      
+    try:
+        balance =  self.accbalance
+        newbalance = int(balance)+int(amount)
+        self.accbalance = newbalance
+        print(f"Your New balance is ${self.accbalance} ")       
+
+        updatedcustomerobj = updateaccountfile(self, cardnumber, newbalance)
+
+        return updatedcustomerobj
+    
+    except Exception as e:
+        print(f"An error occurred within func1-load_depositupdateaccounwupdateaccount(): {e}. Exiting program.")
+
+
+###############ATM program ###################################################
+print(f"*******************************************")
+print(f"WELCOME to Sample ATM: {current_day} {current_month_name.upper()}, {current_year}")
+print(f"*******************************************")
+if __name__ ==  '__main__': atm_demo_proto("")
+############### ATM program end ###################################################
+
